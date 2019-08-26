2ce691e0828347b160b0669f0919f29d47334028
industrious-modal-4420 <khaledhassan45@gmail.com>
2019-08-26 15:44:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,7 +12,7 @@ end
 
 def reduce(source_array, starting = 0)
   i = 0
-  run_total = source_array.all? {|i| i.is_a?(Integer)} ? starting : true
+  run_total = source_array.all? {|i| i.is_a?(Integer)} ? starting : TRUE
   while i < source_array.length do
     n = source_array[i]
     run_total = yield(run_total, n)
